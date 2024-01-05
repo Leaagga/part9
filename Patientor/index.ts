b06e66c255186ff6aefba20a0b939426fa9ebc18
Leaagga <135800909+Leaagga@users.noreply.github.com>
2024-01-05 12:05:29 +0800

    9.10: Patientor backend, step3

--- Patientor/index.ts
+++ Patientor/index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 const app = express();
 app.use(express.json());
-
+import diagnosesRouter from './src/routes/diagnosesRoutes';
 const PORT = 3000;
-
+app.use('/api/diagnoses',diagnosesRouter);
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
